befc7efa85e34c041986a24c4eeaf7e6dab0af53
EJSalazar0000 <edgar.jesus.salazar@gmail.com>
2026-02-01 22:36:29 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jalai",
+    valentineName: "Daniela Jimenez",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much, Babiiii?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW Esto si es lo que necesitaba 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Me encantas, sigue asi! 🚀💝",              // Shows when they go past 1000%
+        normal: "Sigue sigue! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your flowers, a big warm hug, and a huge kiss. See you Tomorrow Mahal!",
+        title: "Yeah! Sabia que aceptatias estar conmigo! 🎉💝💖💝💓",
+        message: "Ven por tus besos y abrazos. Te veo mañana Amor!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
